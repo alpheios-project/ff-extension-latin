479904771d16bc31549ae0025829a81dafffa501
BridgetAlmas <BridgetAlmas@6cdfca17-2474-42dc-bd0f-2d6b4f5baa86>
2008-12-08 19:41:59 +0000

    Bug 152 - removing now obsolete 'words' parameter to the inflection table window (term and suffix are picked up from the alph-entry object directly in the inflections xslt)

--- language-extensions/latin/trunk/content/alpheios-latin-langtool.js
+++ language-extensions/latin/trunk/content/alpheios-latin-langtool.js
@@ -98,14 +98,12 @@ Alph.LanguageToolSet.latin.prototype.getInflectionTable = function(a_node, a_par
     // TODO should flip this to be a single object keys on infl_type
     params.suffixes = {};
     params.entries = {};
-    params.words = {};
     
     for (var infl_type in Alph.LanguageToolSet.latin.INFLECTION_MAP )
     {
         var key = Alph.LanguageToolSet.latin.INFLECTION_MAP[infl_type].keys[0];
         params.suffixes[key] = [];
         params.entries[key] = [];
-        params.words[key] = [];
     }
         
     // The word will have one more more alph-infl-set elements
@@ -208,7 +206,6 @@ Alph.LanguageToolSet.latin.prototype.getInflectionTable = function(a_node, a_par
                     params.entries[infl_type].push(
                         Alph.$(this).parent(".alph-entry").get(0));
                         
-                    params.words[infl_type][word]=1;
 
                     // duplicate suffixes if necessary
                     var num_pofs = Alph.LanguageToolSet.latin.INFLECTION_MAP[pofs].keys.length; 
